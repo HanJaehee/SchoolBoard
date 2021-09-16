40ba8d0756105334c31e1a7e9ad24ecfa476a234
hindsight <hindsight@kakao.com>
2021-09-17 02:17:00 +0900

    init: init School Board Project

---
+++ src/main/java/com/hindsight/sb/SchoolBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hindsight.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hindsight/sb/SchoolBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hindsight.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
